c0a226bc238f6080e4a1fe8db4e4e4891d4be7ca
phil <phil.pondo@gmail.com>
2020-06-03 15:19:18 -0400

    adds middle name and bio to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,19 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%m-%e-%y %H:%M")
+  end
+
+  def full_name
+    if middle_name.nil? || middle_name == ""
+      "#{first_name} #{last_name}"
+    else
+      "#{first_name} #{middle_name} #{last_name}"
+    end
+  end
+
+  def jpn_phone_number
+    "+81 " + phone_number
+  end
+  
 end
